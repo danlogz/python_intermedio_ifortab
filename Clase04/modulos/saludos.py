1745969d30b72945b802d75e4a761544bee6eaca
danlogz <danlog90@gmail.com>
2022-05-11 19:35:49 -0500

    Importando funciones

---
+++ Clase04/modulos/saludos.py
@@ -0,0 +1,10 @@
+def saludo():
+    print('Hola')
+
+def saludo_nombre():
+    print(f'hola {nombre}')
+
+def saludo_nombre_completo(nombre, parterno, materno = ''):
+    print(f'Hola, {nombre}, {parterno}, {materno}')
+
+nombre = 'nombre'
